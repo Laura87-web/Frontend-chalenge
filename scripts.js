fa9a36ae694d518be63944e7ae00decb0553eceb
Wen <lizen777@gmail.com>
2021-06-07 15:29:38 -0300

    final save

--- scripts.js
+++ scripts.js
@@ -12,12 +12,31 @@ $(document).ready(function(){
       $(".hidden-hi").show();
       });
 
+      $("#menuItem-3").mouseover(function(){
+        $("h5").addClass("yellow");
+       
+      });
+
+
 });
 
 
   $(document).ready(function(){
-    $("#menuItem-3").mouseover(function(){
-      $("h5").addClass("yellow");
+    var count = 1;
+    $(".icon-menu").click(function(){
+      if(count === 1){
+        $(".show-hidde").animate({
+          left: "0"
+        })
+        count = 0;
+
+      }else{
+        $(".show-hidde").animate({
+          left: "-100%"
+        })
+        count = 1;
+      }
+      
      
     });
   });
